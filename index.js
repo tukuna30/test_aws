72b5f193ca65ebc32a7ee5feeb7cb3d25d4ba240
t_patrt <tukuna.patro@autodesk.com>
2017-08-12 14:33:31 +0530

    add index.html

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('HEY!')
-});
+// app.get('/', (req, res) => {
+//   res.send('HEY!')
+// });
+app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
