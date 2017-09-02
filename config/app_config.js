34f6ceae608097df3834e19964d3c71120b95b02
t_patrt <tukuna.patro@autodesk.com>
2017-09-02 20:58:52 +0530

    fix envirornment config

--- config/app_config.js
+++ config/app_config.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {};
 APP_CONFIG.PORT = 3000;
-APP_CONFIG.baseUrl = (process.env.dev ? "localhost" :
+APP_CONFIG.baseUrl = "http://" + (process.env.dev ? "localhost" :
     "ec2-13-126-178-201.ap-south-1.compute.amazonaws.com") + ":" + APP_CONFIG.PORT;
 
 exports.appConfig = APP_CONFIG;
