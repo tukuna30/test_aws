2a3dd243ebc9ff39fcb0ec554f99503906b29872
t_patrt <tukuna.patro@autodesk.com>
2017-09-26 23:42:55 +0530

    add book templates

--- assets/views/books.js
+++ assets/views/books.js
@@ -1,14 +1,14 @@
 (function (window, trythings, module) {
     trythings[module] = function() {
         console.log('executing script for books template');
-        let bookCategories = ["Fiction", "Non fiction", "Biography", "Autography", "Self help", "Romance", "Guides", "Sci Fi", "Love Stories"];
+        let bookCategories = ["Fiction", "Non fiction", "Biography", "Autobiography", "Self help", "Romance", "Guides", "Sci Fi", "Love Stories"];
 
         bookCategories.forEach(function(item, i) {
-            let listItem = document.createElement('li'), stateElement = document.createElement('state');
-            stateElement.setAttribute("url", "/books/"+ item.split(' ').join('_'));
+            let listItem = document.createElement('li'), stateElement = document.createElement('state'), itemName = item.split(' ').join('_').toLowerCase();
+            stateElement.setAttribute("url", "/books/"+ itemName);
             stateElement.setAttribute('stateUrl', "books/:categoryId");
             stateElement.setAttribute("name", "booksCategory");
-            stateElement.setAttribute("templateUrl", "assets/views/books/" + item + ".html");
+            stateElement.setAttribute("templateUrl", "assets/views/books/" + itemName + ".html");
             stateElement.append(document.createTextNode(item));
             listItem.append(stateElement)
             document.getElementById('books').append(listItem);
