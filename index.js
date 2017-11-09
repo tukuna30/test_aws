98914eb7adac575a6d352419228e0cc9f673ea21
t_patrt <tukuna.patro@autodesk.com>
2017-11-09 23:07:52 +0530

    provide path to secure keys

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 const cookie = require('cookie');
 let socketConnectionStore = [];
 let options = {
-  key: fs.readFileSync('./server.key'),
-  cert: fs.readFileSync('./server.crt')
+  key: fs.readFileSync((process.env.HOME || process.env.USERPROFILE) + '/.ssh/privkey.pem'),
+  cert: fs.readFileSync((process.env.HOME || process.env.USERPROFILE) + '/.ssh/fullchain.pem')
 };
 let https = require('https').createServer(options, app);
 let User = require('./models/user').User;
