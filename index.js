2a719938ed8376bbd6534931bf228d7ebcbe0092
t_patrt <tukuna.patro@autodesk.com>
2018-03-11 22:36:38 +0530

    update openssl certificates

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 const cookie = require('cookie');
 let socketConnectionStore = [];
 let options = {
-  key: fs.readFileSync((process.env.HOME || process.env.USERPROFILE) + '/.ssh/privkey.pem'),
-  cert: fs.readFileSync((process.env.HOME || process.env.USERPROFILE) + '/.ssh/fullchain.pem')
+  key: fs.readFileSync((process.env.HOME || process.env.USERPROFILE) + '/.ssh/server.key'),
+  cert: fs.readFileSync((process.env.HOME || process.env.USERPROFILE) + '/.ssh/server.crt')
 };
 let https = require('https').createServer(options, app);
 let User = require('./models/user').User;
